a15a0664aa123e85bfe8f7905f58ff9288b63732
Lucas Kalks <lucaskalks00@gmail.com>
2024-08-05 13:31:49 -0300

    app configurado e layout de confirmação realizado

--- processaEnvio.php
+++ processaEnvio.php
@@ -16,6 +16,7 @@ class Mensagem
   private $para = null;
   private $assunto = null;
   private $mensagem = null;
+  public $status = array('codigo_status' => null, 'descricao_status' => null);
   public function __construct($para, $assunto, $mensagem)
   {
     $this->para = $para;
@@ -46,7 +47,7 @@ class Mensagem
 $mensagem = new Mensagem($_POST['para'], $_POST['assunto'], $_POST['mensagem']);
 if (!$mensagem->mensagemValida()) {
   echo 'Mensagem inválida. Falta(m) um ou mais parâmetros';
-  die();
+  header('Location: index.php');
 }
 
 //Create an instance; passing `true` enables exceptions
@@ -54,7 +55,7 @@ $mail = new PHPMailer(true);
 
 try {
   //Server settings
-  $mail->SMTPDebug  = SMTP::DEBUG_SERVER;                     //Enable verbose debug output
+  $mail->SMTPDebug  = false;    //SMTP::DEBUG_SERVER;                     //Enable verbose debug output
   $mail->isSMTP();                                            //Send using SMTP
   $mail->Host       = 'smtp.gmail.com';                       //Set the SMTP server to send through
   $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
@@ -83,7 +84,50 @@ try {
   $mail->AltBody = $mensagem->__get('mensagem');        //Corpo do e-mail no padrão Não-HTML
 
   $mail->send();
-  echo 'Mensagem Enviada!';
+  $mensagem->status['codigo_status'] = 1;
+  $mensagem->status['descricao_status'] = 'Mensagem Enviada!';
 } catch (Exception $e) {
-  echo "Não foi possível enviar este email! Por favor, tente mais tarde. Erro: {$mail->ErrorInfo}";
+  $mensagem->status['codigo_status'] = 2;
+  $mensagem->status['descricao_status'] = "Não foi possível enviar este email! Por favor, tente mais tarde. Erro: {$mail->ErrorInfo}";
 }
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>App Send Mail</title>
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+  <div class="container">  
+			<div class="py-3 text-center">
+				<img class="d-block mx-auto mb-2" src="logo.png" alt="" width="72" height="72">
+				<h2>Send Mail</h2>
+				<p class="lead">Seu app de envio de e-mails particular!</p>
+			</div>
+
+      <div class="row">
+        <div class="col-md-12">
+          <?php if($mensagem->status["codigo_status"] === 1) { ?>
+            <div class="container">
+              <h1 class="display-4 text-success">Sucesso!</h1>
+              <p><?= $mensagem->status["descricao_status"]?></p>
+              <a href="index.php" class="btn btn-lg btn-success">Voltar</a>
+            </div>
+          <?php } else if ($mensagem->status["codigo_status"] === 2) { ?>
+            <div class="container">
+              <h1 class="display-4 text-danger">Falha!</h1>
+              <p><?= $mensagem->status["descricao_status"]?></p>
+              <a href="index.php" class="btn btn-lg btn-danger">Voltar</a>
+            </div>
+          <?php } ?>
+        </div>
+      </div>
+  </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
